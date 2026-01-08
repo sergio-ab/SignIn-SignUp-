7c8c6c1ec68049c3d3d629ec1ca7bc0201698a25
clara <clara@2DAW-012>
2026-01-08 10:23:28 +0100

    Inicio HTML y función generadora

---
+++ web/controllerAccounts.js
@@ -0,0 +1,22 @@
+/* 
+<!------------------------------------------------------------------------------
+    |   Autor: Clara Montaño Rodríguez
+    |   Última modificación: 08/01/2026
+--------------------------------------------------------------------------------
+    |   CONTROLLER
+    |   
+    |   
+    |   
+    |   
+------------------------------------------------------------------------------->
+ */
+
+
+function* accountRowGenerator(accounts) {
+    for (const account of accounts) {
+        // Fila (equivalente a <tr>)
+        const row = document.createElement("div");
+        row.className = "grid grid--row";
+    }
+    
+}
